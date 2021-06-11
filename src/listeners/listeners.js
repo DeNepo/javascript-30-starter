e39b916c609b83778d12f1967beb4551d2faaf8b
Oksana Shulha <shushullja@gmail.com>
2021-06-11 21:36:06 +0200

    Add Drum Kit

---
+++ src/listeners/listeners.js
@@ -0,0 +1,6 @@
+import { removeTransition, playSound }  from "../handlers/handlers.js";
+
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
